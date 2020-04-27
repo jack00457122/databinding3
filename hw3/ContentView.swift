509714e9309ad48b55d23832c5c59bbc76dc1b2a
aaaaaaaaaaa <aaaaa>
2020-04-27 17:28:31 +0800

    Initial Commit

---
+++ hw3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw3
+//
+//  Created by User09 on 2020/4/27.
+//  Copyright © 2020 00457122. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
